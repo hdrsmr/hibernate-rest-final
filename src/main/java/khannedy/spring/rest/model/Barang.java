ab38a6eef8ef50010b22b34c18d927e122bd34a4
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2013-08-03 12:23:15 +0700

    create Barang and Status model

---
+++ src/main/java/khannedy/spring/rest/model/Barang.java
@@ -0,0 +1,69 @@
+package khannedy.spring.rest.model;
+
+import java.util.Date;
+
+/**
+ * Created by pakarjava on 8/3/13.
+ */
+public class Barang {
+
+    private String nama;
+
+    private String kategori;
+
+    private Long harga;
+
+    private Integer stok;
+
+    private Boolean mudahTerbakar;
+
+    private Date tanggalKadaluarsa;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public Long getHarga() {
+        return harga;
+    }
+
+    public void setHarga(Long harga) {
+        this.harga = harga;
+    }
+
+    public Integer getStok() {
+        return stok;
+    }
+
+    public void setStok(Integer stok) {
+        this.stok = stok;
+    }
+
+    public Boolean getMudahTerbakar() {
+        return mudahTerbakar;
+    }
+
+    public void setMudahTerbakar(Boolean mudahTerbakar) {
+        this.mudahTerbakar = mudahTerbakar;
+    }
+
+    public Date getTanggalKadaluarsa() {
+        return tanggalKadaluarsa;
+    }
+
+    public void setTanggalKadaluarsa(Date tanggalKadaluarsa) {
+        this.tanggalKadaluarsa = tanggalKadaluarsa;
+    }
+}
---
+++ src/main/java/khannedy/spring/rest/model/Status.java
@@ -0,0 +1,27 @@
+package khannedy.spring.rest.model;
+
+/**
+ * Created by pakarjava on 8/3/13.
+ */
+public class Status {
+
+    private String kode;
+
+    private String pesan;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getPesan() {
+        return pesan;
+    }
+
+    public void setPesan(String pesan) {
+        this.pesan = pesan;
+    }
+}
